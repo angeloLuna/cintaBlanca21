fdbae4ffa90f3ddc5e344f583c3072eb99617448
angeloLuna <angel.luna.najera@gmail.com>
2018-07-05 21:48:41 -0500

    Conectar html con js usando bootstrap

---
+++ conectHTML-JS/main.js
@@ -0,0 +1,11 @@
+let nombre = prompt("nombre")
+let apellido = prompt("apellido")
+let descripcion = prompt("descripción")
+
+let cardNombre = document.getElementById("nombre");
+let cardApellido = document.getElementById("apellido");
+let cardDescripcion = document.getElementById("descripcion");
+
+cardNombre.innerHTML = nombre;
+cardApellido.innerHTML = apellido;
+cardDescripcion.innerHTML = descripcion;
